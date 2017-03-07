8dbeef11ee118ce1e6ed29f7451eeea5c26be8c1
piouslove <305053045@qq.com>
2017-03-07 20:41:50 +0800

    配置文件

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'piouslove'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'myblog.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'myblog.db')#sqlite数据库地址
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
